ce1ccea6d9827501ec3ee0ef95515b435ba5efc2
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-16 23:42:54 +0545

    4th push anish

--- src/component/home/Skill.jsx
+++ src/component/home/Skill.jsx
@@ -3,14 +3,23 @@ import React from "react";
 const Skill = () => {
   const data = [
     {
-      title: "Every Day is a New Challenge",
+      tit: "Every Day is a New Challenge",
       para: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id autem veritatis nobis inventore consectetur omnis porro iusto excepturi, quo voluptate quos vel, quasi officia fugiat.",
       para1:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos illo qui quibusdam perspiciatis soluta cumque quia asperiores sapiente! Veniam odit veritatis mollitia eos similique impedit beatae iusto.",
     },
   ];
 
-  const Progress = () => {};
+  const Progress = ({ done, title }) => {
+    return (
+      <div className="progress">
+        <div className="progress-done" style={{ opacity: 1, width: `${done}` }}>
+          <h4>{title}</h4>
+          <h4>{done}%</h4>
+        </div>
+      </div>
+    );
+  };
 
   return (
     <>
@@ -22,7 +31,7 @@ const Skill = () => {
           </div>
           <div className="content flex">
             <div className="left topMargin">
-              <Progress />
+              <Progress done="70" title="HTML" />
             </div>
             <div className="right"></div>
           </div>
