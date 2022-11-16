827bef4a778be1a0c581f44571a51a6ac9b1e5c1
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-16 23:52:40 +0545

    4th push anish

--- src/component/home/Skill.jsx
+++ src/component/home/Skill.jsx
@@ -35,6 +35,9 @@ const Skill = () => {
           <div className="content flex">
             <div className="left topMargin">
               <Progress done="70" title="HTML" />
+              <Progress done="80" title="CSS" />
+              <Progress done="80" title="JAVASCRIPT" />
+              <Progress done="90" title="REACT" />
             </div>
             <div className="right"></div>
           </div>
